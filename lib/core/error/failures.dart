a3bd7f755df335aae5d69afeec2aa4b7432a0bf7
Seuketchup <123664244+Seuketchup@users.noreply.github.com>
2023-10-31 10:59:00 +0800

    Added model

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -5,4 +5,8 @@ abstract class Failure extends Equatable {
   @override
   // TODO: implement props
   List<Object?> get props => [];
+
 }
+
+class ServerFailure extends Failure {}
+class CacheFailure extends Failure {}
